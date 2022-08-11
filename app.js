01dea5fbe5b86a6d167e8a705ed10fa0061f70fb
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-08-11 00:01:00 -0400

    added hamburger

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+    // Hamburger menu-btn
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
+
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");  
+        navMenu.classList.remove("active");  
+    }))
+
+    
     $(window).scroll(function(){
 
         if(this.scrollY > 200){
---
+++ script.js
@@ -0,0 +1,4 @@
+    // Hamburger menu-btn
+
+    const menu-btn = document.querySelector(".menu-btn");
+    const menu = document.querySelector(".menu");
